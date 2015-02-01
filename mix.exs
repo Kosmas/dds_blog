d84b55d5a3d60ae1c1e8414cf1c22505075a596f
Kosmas <kosmas@mach7x.com>
2015-02-01 12:00:36 +0000

    added cowboy dependency

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule DdsBlog.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger],
+    [applications: [:logger, :cowboy],
      mod: {DdsBlog, []}]
   end
 
@@ -26,6 +26,6 @@ defmodule DdsBlog.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:cowboy, "~> 1.0.0"}]
   end
 end
